f7e1cd29b23dc54426e476995cff6ccd2feb07fb
mdalrasel <mdalrasel.dev@gmail.com>
2025-03-01 19:19:02 +0600

    card create

---
+++ script/main.js
@@ -0,0 +1,6 @@
+document.getElementById("date").innerText = new Date().toLocaleDateString();
+
+document.getElementById("blog").addEventListener("click", 
+    function(){
+        window.location("Blog.html")
+    })
